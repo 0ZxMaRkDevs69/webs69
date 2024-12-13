7c7bcb27e185eca734326f23bd10065940d1bc92
C3aZ4ar0019 <jc8374356@gmail.com>
2024-12-14 05:38:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 //const axios = require('axios');
-//const path = require('path');
+const path = require('path');
 const app = express();
 app.use(express.json());
 /*
